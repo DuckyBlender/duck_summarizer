0f851997c533f5a9bca3594e1ef5dfc2d6bac26c
DuckyBlender <alan@alanklas.com>
2025-02-25 11:59:51 +0100

    update script

--- start.sh
+++ start.sh
@@ -1,2 +1,22 @@
+#!/bin/bash
+
+# Build the Docker image
+echo "Building Docker image duck_summarizer..."
 docker build -t duck_summarizer .
+
+# Find and stop any previous containers running the duck_summarizer image
+echo "Stopping any previous duck_summarizer containers..."
+PREV_CONTAINER=$(docker ps -q --filter ancestor=duck_summarizer)
+
+if [ -n "$PREV_CONTAINER" ]; then
+  echo "Found previous container: $PREV_CONTAINER"
+  docker stop $PREV_CONTAINER
+  echo "Previous container stopped"
+else
+  echo "No previous container found"
+fi
+
+# Run a new container in detached mode
+echo "Starting new duck_summarizer container..."
 docker run -d duck_summarizer
+echo "Container started"
