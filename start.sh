f48cdfc2f7b3e0b4e26139462a9943e52fd9ee8e
DuckyBlender <alan@alanklas.com>
2025-02-25 12:14:52 +0100

    fix script

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 docker stop $(docker ps -q --filter ancestor=duck_summarizer) || true
 docker build -t duck_summarizer .
-
-docker run -d duck_summarizer# Build the Docker image
+docker run -d duck_summarizer
